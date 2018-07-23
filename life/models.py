3f657e6b49c9b8fa26ba024a551bddfd8b083837
宋玮 <david@Davids-MacBook-Pro-17.local>
2018-07-22 21:30:39 -0700

    inition

--- life/models.py
+++ life/models.py
@@ -3,3 +3,13 @@ from django.db import models
 class Group(models.Model):
 	established = models.DateTimeField(auto_now_add=True)
 	name = models.CharField(max_length=50)
+
+
+class Music_Playlist(models.Model):
+	album_name = models.CharField(max_length=50)
+	artist = models.CharField(max_length=50)
+
+class Food_Order(models.Model):
+	name = models.CharField(max_length=50)
+	food = models.CharField(max_length=50)
+	count = models.IntegerField()
--- life/views.py
+++ life/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.core import serializers
+from life.models import *
 
 def index(request):
-    return render(request, 'life/index.html')
+	all_food_orders = Food_Order.objects.all()
+	all_music_playlists = Music_Playlist.objects.all()
+	return render(request, 'life/index.html', {"musics": all_music_playlists, "orders": all_food_orders})
